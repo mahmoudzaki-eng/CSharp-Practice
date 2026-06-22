f8798d9ed89a648d47ef48f4f2fc006a233a67bd
mahmoudzaki-eng <md692005@gmail.com>
2026-06-22 02:20:09 -0700

    Add project files.

---
+++ learnC#ForBiggner/Employee.cs
@@ -0,0 +1,37 @@
+﻿using EmployeeSalaryUpdate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learnC_ForBiggner
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Salary { get; set; }
+
+        public int Experience { get; set; }
+
+        public static void UpdateSalary(
+            Employee[] employees,
+            EmployeeCondition condition)
+        {
+            foreach (Employee employee in employees)
+            {
+                if (condition(employee))
+                {
+                    employee.Salary += employee.Salary * 0.10; // زيادة 10%
+
+                    Console.WriteLine(
+                        $"Employee: {employee.Name} | " +
+                        $"New Salary: {employee.Salary}");
+                }
+            }
+        }
+    }
+}
---
+++ learnC#ForBiggner/Program.cs
@@ -0,0 +1,34 @@
+﻿using learnC_ForBiggner;
+using System;
+
+namespace EmployeeSalaryUpdate
+{
+    // Delegate
+    public delegate bool EmployeeCondition(Employee employee);
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] employees = 
+            {
+                new Employee { Id = 1, Name = "Mahmoud", Salary = 40000, Experience = 6 },
+                new Employee { Id = 2, Name = "Mohamed", Salary = 60000, Experience = 2 },
+                new Employee { Id = 3, Name = "Zaki", Salary = 80000, Experience = 3 },
+                new Employee { Id = 4, Name = "Ali", Salary = 100000, Experience = 5 }
+            };
+
+            EmployeeCondition condition = IsEligibleForUpdate;
+
+            Employee.UpdateSalary(employees, condition);
+
+            Console.ReadKey();
+        }
+
+        // شرط التحديث
+        public static bool IsEligibleForUpdate(Employee employee)
+        {
+            return employee.Experience >= 5;
+        }
+    }
+}
